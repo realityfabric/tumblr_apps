77b13c98796614dc7b285d58b9e402be1437289b
realityfabric <monstergoddess@users.noreply.github.com>
2015-08-01 00:28:22 -0700

    i can't post to tumblr via the browser for some reason, so i wrote a command line posting function that works around that fact

---
+++ posts.js
@@ -0,0 +1,34 @@
+var t_app = require('./main');
+
+var Blog = "";
+var Body = "";
+var Tags = "";
+var errflag = false;
+
+if (process.argv[2] != null) {
+	Blog = process.argv[2];
+	if (process.argv[3] != null) {
+		Body = process.argv[3];
+		if (process.argv[4] != null) {
+			Tags = process.argv[4];
+		} else {
+			Tags = "";
+		}
+	} else {
+		errflag = true;
+	}
+} else {
+	errflag = true;
+}
+
+if (errflag) {
+	console.log ("Format is: nodejs posts.js [blogname] '[post body]' '[comma separated list of tags]'");
+} else {
+		t_app.client.text(Blog, {  body: Body,
+									tweet: 'off',
+									format: 'markdown',
+									tags: Tags}, function (err, success) {
+		console.log(success);
+	});
+}
+
