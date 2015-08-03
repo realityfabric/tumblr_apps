9f0b516f8d6aa940a3fb77c4d38ab241c55a1408
realityfabric <gaymoongoddess@gmail.com>
2015-08-03 00:51:37 -0700

    Took out the console log in the download function

--- dashboard.js
+++ dashboard.js
@@ -471,12 +471,8 @@ var parseHTML = function (str) {
 var download = function(uri, filename, callback){
 
   request.head(uri, function(err, res, body){
-
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    var r = request(uri).pipe(fs.createWriteStream(filename));
-    r.on('close', callback);
+	  var r = request(uri).pipe(fs.createWriteStream(filename));
+	  r.on('close', callback);
   });
 };
 
